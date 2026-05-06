873a852eca9912eeda2dc9906ba24b88bd42ad67
NickollasKaiky <kainickollas@gmail.com>
2026-05-06 14:03:07 -0300

    Update pokemon.php

--- pokemon.php
+++ pokemon.php
@@ -14,7 +14,7 @@ class Pokemon{
     {
      $this->nome = $nome;
      $this->tipo = $tipo;
-     $this->xp = 120;
+     $this->xp = 0;
      $this->pv = $pv;
      $this->ataque = $ataque;
      $this->defesa = $defesa;
@@ -23,25 +23,34 @@ class Pokemon{
     }
    
     function batalhar(){
+        print "\033c";
+        print "Resultado da batalha:\n";
+        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
         if (rand(1,100) > 50) {
-            print "Você VENCEU!!!!";
+            print "\n\tVocê VENCEU!!!!\n\n";
+        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
             print $this->nome . " recebeu " . $this->aumentar_xp() . "XP\n";
-            print "Ganhou " . $this->aumentar_nivel() . " level";
-            print "Aumentou PV para " . $this->aumentar_pv() . "PV" ;
+            print "Level: " . $this->aumentar_nivel() . "\n";
+            $this->xp = 0;
+            print "PV aumentou para " . $this->aumentar_pv() . "PV\n" ;
 
         }
         else{
-            print "Você PERDEU!!!";
+            print "\n\tVocê PERDEU!!!\n\n";
         }
     }
 
     function aumentar_nivel(){
-       if($this->lv =! ($this->xp / 120));
-        return $this->lv++;
+       if($this->lv =! ($this->xp / 120)){
+        return $this->lv = $this->lv - ($this->xp / 120);
+       }
+       else{
+        return $this->lv;
+       }
     }
 
     function aumentar_pv(){
-        return $this->pv *= ($this->pv * 0.10);
+        return $this->pv += ($this->pv * 0.1);
     }
 
     function aumentar_xp(){
@@ -49,7 +58,15 @@ class Pokemon{
     }
 
     function ver_atributos(){
-    
+        print "\033c";
+        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            Nome: $this->nome
+            Level: $this->lv
+            Tipo: $this->tipo
+            Pontos de Vida: $this->pv
+            Ataque: $this->ataque
+            Defesa: $this->defesa
+            Velocidade: $this->velocidade\n";
     }
 }
 
@@ -68,7 +85,7 @@ $pokemon2 = new Pokemon("Shedinja", "Inseto/Fantasma", 1, 90, 45, 40);
             switch ($opcao) {
                 case 1:
                     print "Selecione um Pokémon
-                    1. $pokemon->nome  |  2. $pokemon->nome ";
+                    1. $pokemon->nome  |  2. $pokemon2->nome \n";
 
                     $opcao = readline();
 
@@ -78,11 +95,32 @@ $pokemon2 = new Pokemon("Shedinja", "Inseto/Fantasma", 1, 90, 45, 40);
                             break;
                         case 2:
                             $pokemon2 -> batalhar();
+                            break;
                         
                         default:
                             print "Pokémon não disponível";
                             break;
                     }
+                    break;
+                case 2:
+                    print "Selecione um Pokémon
+                    1. $pokemon->nome  |  2. $pokemon2->nome \n";
+
+                    $opcao = readline();
+
+                    switch ($opcao) {
+                        case 1:
+                            $pokemon -> ver_atributos();
+                            break;
+                        case 2:
+                            $pokemon2 -> ver_atributos();
+                            break;
+                        
+                        default:
+                            print "Pokémon não disponível";
+                            break;
+                    }
+
                     break;
                 case 3:
                     print "\033c";
@@ -94,5 +132,3 @@ $pokemon2 = new Pokemon("Shedinja", "Inseto/Fantasma", 1, 90, 45, 40);
                     break;
             }
         }
-
-
